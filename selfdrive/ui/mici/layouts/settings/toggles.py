2c2e9aae90eb9511d12bf7af3d9de4e366cfa034
Hendrik Groove <hendrikgroove@gmail.com>
2026-03-30 12:34:37 +0200

    Port Hyundai Radar Tracks (sunnypilot/sunnypilot#1723)
    - HyundaiRadar param: Off / Lead Only / Full Radar
    - opendbc: MRR30/MRR35/MRREVO14F/LRR25 radar DBC + parsing
    - Ioniq 6 tagged as MRR35_RADAR (0x3A5-0x3C4, 32 tracks)
    - UI toggle in settings

--- selfdrive/ui/mici/layouts/settings/toggles.py
+++ selfdrive/ui/mici/layouts/settings/toggles.py
@@ -22,9 +22,11 @@ class TogglesLayoutMici(NavScroller):
     record_mic = BigParamControl("record & upload mic audio", "RecordAudio", toggle_callback=restart_needed_callback)
     enable_openpilot = BigParamControl("enable sunnypilot", "OpenpilotEnabledToggle", toggle_callback=restart_needed_callback)
     radar_tracks = BigParamControl("radar tracks", "RadarTracks")
+    hyundai_radar = BigMultiParamToggle("hyundai radar", "HyundaiRadar", ["off", "lead only", "full radar"])
 
     self._scroller.add_widgets([
       radar_tracks,
+      hyundai_radar,
       self._personality_toggle,
       self._experimental_btn,
       is_metric_toggle,
@@ -38,6 +40,7 @@ class TogglesLayoutMici(NavScroller):
     # Toggle lists
     self._refresh_toggles = (
       ("RadarTracks", radar_tracks),
+      ("HyundaiRadar", hyundai_radar),
       ("ExperimentalMode", self._experimental_btn),
       ("IsMetric", is_metric_toggle),
       ("IsLdwEnabled", ldw_toggle),
